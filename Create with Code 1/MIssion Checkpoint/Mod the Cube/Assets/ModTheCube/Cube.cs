9408b17d5019f73f9c60a05b32e0fd07ec06e193
Fawad Ahmad <fawadshah501@gmail.com>
2023-01-21 00:20:29 +0500

    Add Mission Checkpoint - Mode the Cube

---
+++ Create with Code 1/MIssion Checkpoint/Mod the Cube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.5f;
+    }
+    
+    void Update()
+    {
+        Material material = Renderer.material;
+        material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.5f, 1.0f), Random.Range(0.0f, 1.0f), 0.4f);
+        transform.Rotate(30.0f * Time.deltaTime, 0.0f, 25.0f * Time.deltaTime);
+    }
+}

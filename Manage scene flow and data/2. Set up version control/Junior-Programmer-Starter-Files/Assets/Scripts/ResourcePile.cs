99d65782540bcd79a1e686daa47ce1c9cabc399c
Fawad Ahmad <fawadshah501@gmail.com>
2023-02-12 00:22:42 +0500

    Apply OOP Princicples

--- Manage scene flow and data/2. Set up version control/Junior-Programmer-Starter-Files/Assets/Scripts/ResourcePile.cs
+++ Manage scene flow and data/2. Set up version control/Junior-Programmer-Starter-Files/Assets/Scripts/ResourcePile.cs
@@ -11,7 +11,17 @@ public class ResourcePile : Building
     public ResourceItem Item;
 
     private float m_ProductionSpeed = 0.5f;
-    public float ProductionSpeed;
+    public float ProductionSpeed
+    {
+        get { return m_ProductionSpeed; }
+        set
+        {
+            if (value < 0)
+                Debug.LogError("You can't set a negative production speed!");
+            else
+                m_ProductionSpeed = value;
+        }
+    }
 
     private float m_CurrentProduction = 0.0f;
 
@@ -24,18 +34,18 @@ public class ResourcePile : Building
 
             m_CurrentProduction = m_CurrentProduction - amountToAdd + leftOver;
         }
-        
+
         if (m_CurrentProduction < 1.0f)
         {
-            m_CurrentProduction += ProductionSpeed * Time.deltaTime;
+            m_CurrentProduction += m_ProductionSpeed * Time.deltaTime;
         }
     }
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
-        
+        return $"Producing at the speed of {m_ProductionSpeed}/s";
+
     }
-    
-    
+
+
 }
